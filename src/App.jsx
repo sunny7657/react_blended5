491163b8c410ceb4e307857712d84758a5584fd9
sunny7657 <ksvetlana466@gmail.com>
2024-03-02 14:17:12 +0100

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
-import { Heading } from 'components';
+import { Header } from 'components';
+import { lazy, useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { getUserInfo } from './service';
+
+const Home = lazy(() => import('./pages/Home'));
+const Rates = lazy(() => import('./pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    function success(pos) {
+      console.log(pos);
+      getUserInfo(pos.coords).then(console.log);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
